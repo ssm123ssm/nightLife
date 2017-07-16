5173174bf9e1db1159a010b38c8bd66fc7de3d3c
ssm123ssm <ssm123ssm@gmail.com>
2017-07-17 01:04:30 +0530

    fixed mongo err

--- app/config/databases.js
+++ app/config/databases.js
@@ -1,4 +1,4 @@
 module.exports = {
-    dbURL: 'mongodb://ssm123ssm:candrani123@ds157702.mlab.com:57702/night',
-    usersURL: 'mongodb://ssm123ssm:candrani123@ds157702.mlab.com:57702/night'
+    dbURL: 'mongodb://ssm123ssm:chandrani123@ds157702.mlab.com:57702/night',
+    usersURL: 'mongodb://ssm123ssm:chandrani123@ds157702.mlab.com:57702/night'
 }
